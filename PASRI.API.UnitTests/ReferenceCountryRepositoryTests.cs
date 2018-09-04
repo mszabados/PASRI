68dc06dba22697410a68af9beeaf93de5ccc139c
Darryl Schott <torrerre@yahoo.com>
2018-09-04 07:52:02 -0400

    Completed tests for ReferenceCountryRepository

--- PASRI.API.UnitTests/ReferenceCountryRepositoryTests.cs
+++ PASRI.API.UnitTests/ReferenceCountryRepositoryTests.cs
@@ -1,7 +1,10 @@
-using System;
-using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using PASRI.API.Core.Domain;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace PASRI.API.UnitTests
 {
@@ -35,6 +38,232 @@ namespace PASRI.API.UnitTests
             var result = UnitOfWork.ReferenceCountries.Get(invalidCountryCode);
 
             Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void Find_PredicateUsedToFindOneCountry_ReturnsCollectionWithOneCountry()
+        {
+            Expression<Func<ReferenceCountry, bool>> predicate =
+                (p => p.StartDate <= new DateTime(1800, 1, 1));
+            var result = UnitOfWork.ReferenceCountries.Find(predicate);
+
+            Assert.That(result.Count, Is.EqualTo(1));
+        }
+
+        [Test]
+        public void Find_PredicateUsedToFindMoreThanOneCountry_ReturnsCollectionWithOneCountry()
+        {
+            Expression<Func<ReferenceCountry, bool>> predicate =
+                (p => p.StartDate == null);
+            var result = UnitOfWork.ReferenceCountries.Find(predicate);
+
+            Assert.That(result.Count, Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Find_PredicateUsedToFindNoCountries_ReturnsEmptyCollection()
+        {
+            Expression<Func<ReferenceCountry, bool>> predicate =
+                (p => p.StartDate > new DateTime(1800, 1, 1));
+            var result = UnitOfWork.ReferenceCountries.Find(predicate);
+
+            Assert.That(result.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void SingleOrDefault_PredicateUsedToFindOneCountry_ReturnsOneCountry()
+        {
+            Expression<Func<ReferenceCountry, bool>> predicate =
+                (p => p.StartDate <= new DateTime(1800, 1, 1));
+            var result = UnitOfWork.ReferenceCountries.SingleOrDefault(predicate);
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void SingleOrDefault_PredicateUsedToFindMoreOneCountry_ThrowsInvalidOperationException()
+        {
+            Expression<Func<ReferenceCountry, bool>> predicate =
+                (p => p.StartDate == null);
+
+            Assert.That(() => 
+                UnitOfWork.ReferenceCountries.SingleOrDefault(predicate), 
+                Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void SingleOrDefault_PredicateUsedOnToFindNoCountries_ReturnsNull()
+        {
+            Expression<Func<ReferenceCountry, bool>> predicate =
+                (p => p.StartDate > new DateTime(1800, 1, 1));
+            var result = UnitOfWork.ReferenceCountries.SingleOrDefault(predicate);
+
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void Add_ValidCountryNotExists_FetchNewCountry()
+        {
+            string testCountryCode = "ZZ";
+            UnitOfWork.ReferenceCountries.Add(
+                new ReferenceCountry() {
+                    Code = testCountryCode,
+                    DisplayText = testCountryCode
+                });
+            UnitOfWork.Complete();
+
+            var result = UnitOfWork.ReferenceCountries.Get(testCountryCode);
+
+            Assert.That(result, Is.Not.Null);
+        }
+
+        [Test]
+        public void Add_ValidCountryExists_ThrowsInvalidOperationException()
+        {
+            Assert.That(() => UnitOfWork.ReferenceCountries.Add(
+                new ReferenceCountry()
+                {
+                    Code = "US",
+                    DisplayText = ""
+                }),
+                Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void Add_InvalidCountry_ThrowsDbUpdateException()
+        {
+            UnitOfWork.ReferenceCountries.Add(new ReferenceCountry());
+
+            Assert.That(() => UnitOfWork.Complete(), 
+                Throws.TypeOf<DbUpdateException>());
+        }
+
+        [Test]
+        public void AddRange_ValidCountries_FetchNewCountry()
+        {
+            var newCountries = new Collection<ReferenceCountry>
+            {
+                new ReferenceCountry() { Code = "ZZ", DisplayText = "" },
+                new ReferenceCountry() { Code = "ZY", DisplayText = "" }
+            };
+            UnitOfWork.ReferenceCountries.AddRange(newCountries);
+            UnitOfWork.Complete();
+
+            var result = UnitOfWork.ReferenceCountries.GetAll();
+
+            Assert.That(result.Count, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void AddRange_ValidCountriesDuplicated_ThrowsInvalidOperationException()
+        {
+            var newCountries = new Collection<ReferenceCountry>
+            {
+                new ReferenceCountry() { Code = "ZZ", DisplayText = "" },
+                new ReferenceCountry() { Code = "ZZ", DisplayText = "" }
+            };
+
+            Assert.That(() => UnitOfWork.ReferenceCountries.AddRange(newCountries),
+                Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void AddRange_InvalidCountries_ThrowsDbUpdateException()
+        {
+            var newCountries = new Collection<ReferenceCountry>
+            {
+                new ReferenceCountry(),
+                new ReferenceCountry()
+            };
+            UnitOfWork.ReferenceCountries.AddRange(newCountries);
+
+            Assert.That(() =>
+                UnitOfWork.Complete(), 
+                Throws.TypeOf<DbUpdateException>());
+        }
+
+        [Test]
+        public void Remove_ValidCountryNotExists_ThrowsDbUpdateConcurrencyException()
+        {
+            string testCountryCode = "ZZ";
+            UnitOfWork.ReferenceCountries.Remove(
+                new ReferenceCountry() {
+                    Code = testCountryCode,
+                    DisplayText = testCountryCode
+                });
+
+            Assert.That(() => UnitOfWork.Complete(),
+                Throws.TypeOf<DbUpdateConcurrencyException>());
+        }
+
+        [Test]
+        public void Remove_ValidCountryExists_CountryCannotBeFetched()
+        {
+            string validCountryCode = "US";
+            var removeReferenceCountry = UnitOfWork.ReferenceCountries.Get(validCountryCode);
+            UnitOfWork.ReferenceCountries.Remove(removeReferenceCountry);
+            UnitOfWork.Complete();
+
+            var result = UnitOfWork.ReferenceCountries.Get(validCountryCode);
+
+            Assert.That(result, Is.Null);
+        }
+
+        [Test]
+        public void Remove_InvalidCountry_ThrowsDbUpdateConcurrencyException()
+        {
+            UnitOfWork.ReferenceCountries.Remove(new ReferenceCountry());
+
+            Assert.That(() => UnitOfWork.Complete(),
+                Throws.TypeOf<DbUpdateConcurrencyException>());
+        }
+
+        [Test]
+        public void RemoveRange_TwoValidCountries_OneCountryRemains()
+        {
+            var referenceCountries = UnitOfWork.ReferenceCountries.GetAll().ToList();
+            var removeReferenceCountries = new Collection<ReferenceCountry>();
+            var removeCount = 2;
+            for (int i = 0; i < removeCount; i++)
+            {
+                removeReferenceCountries.Add(referenceCountries.ElementAt(i));
+            }
+
+            UnitOfWork.ReferenceCountries.RemoveRange(removeReferenceCountries);
+            UnitOfWork.Complete();
+
+            var result = UnitOfWork.ReferenceCountries.GetAll();
+
+            Assert.That(result.Count, Is.EqualTo(referenceCountries.Count - removeCount));
+        }
+
+        [Test]
+        public void RemoveRange_ValidCountriesDuplicated_ThrowsInvalidOperationException()
+        {
+            var newCountries = new Collection<ReferenceCountry>
+            {
+                new ReferenceCountry() { Code = "US", DisplayText = "" },
+                new ReferenceCountry() { Code = "US", DisplayText = "" }
+            };
+
+            Assert.That(() => UnitOfWork.ReferenceCountries.RemoveRange(newCountries),
+                Throws.InvalidOperationException);
+        }
+
+        [Test]
+        public void RemoveRange_InvalidCountries_DbUpdateConcurrencyException()
+        {
+            var removeReferenceCountries = new Collection<ReferenceCountry>
+            {
+                new ReferenceCountry(),
+                new ReferenceCountry()
+            };
+
+            UnitOfWork.ReferenceCountries.RemoveRange(removeReferenceCountries);
+
+            Assert.That(() =>
+                UnitOfWork.Complete(), 
+                Throws.TypeOf<DbUpdateConcurrencyException>());
         }
 
         protected override void LoadTestData()
