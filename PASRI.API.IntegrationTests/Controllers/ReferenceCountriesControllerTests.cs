105cdd0c6b1eb3df5f017bf0004fda558d034a05
Darryl Schott <torrerre@yahoo.com>
2018-09-05 14:52:17 -0400

    Added two more test methods to ReferenceCountriesControllerTests for a single country and not found.

--- PASRI.API.IntegrationTests/Controllers/ReferenceCountriesControllerTests.cs
+++ PASRI.API.IntegrationTests/Controllers/ReferenceCountriesControllerTests.cs
@@ -1,13 +1,14 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using NUnit.Framework;
+using PASRI.API.Controllers;
 using PASRI.API.Core.Domain;
 using PASRI.API.Dtos;
 using PASRI.API.TestHelper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
 
 namespace PASRI.API.IntegrationTests.Controllers
 {
@@ -15,7 +16,7 @@ namespace PASRI.API.IntegrationTests.Controllers
     public class ReferenceCountriesControllerTests : BaseIntegrationTestProvider
     {
         private string _apiRelativePath =
-            $"/api/{typeof(ReferenceCountriesControllerTests).Name.Replace("ControllerTests", "")}";
+            $"/api/{nameof(ReferenceCountriesController).Replace("Controller", "")}";
 
         [Test]
         public async Task GetAll_WhenCalled_HttpOkAndReturnsCollection()
@@ -26,6 +27,7 @@ namespace PASRI.API.IntegrationTests.Controllers
             Assert.DoesNotThrow(
                 () => response.EnsureSuccessStatusCode(),
                 String.Format(HttpExceptionFormattedMessage, responseString));
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
             IEnumerable<ReferenceBaseDto> queriedCountries = 
                 JsonConvert.DeserializeObject<IEnumerable<ReferenceBaseDto>>(responseString);
@@ -46,6 +48,7 @@ namespace PASRI.API.IntegrationTests.Controllers
             Assert.DoesNotThrow(
                 () => response.EnsureSuccessStatusCode(),
                 String.Format(HttpExceptionFormattedMessage, responseString));
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
 
             ReferenceBaseDto queriedCountry =
                 JsonConvert.DeserializeObject<ReferenceBaseDto>(responseString);
@@ -55,6 +58,15 @@ namespace PASRI.API.IntegrationTests.Controllers
                     .SingleOrDefault(c => c.Code == validCountryCode);
 
             Helper.AreObjectsEqual(queriedCountry, Mapper.Map<ReferenceCountry, ReferenceBaseDto>(preDefinedCountry));
+        }
+
+        [Test]
+        public async Task Get_InvalidCountryCode_HttpNotFound()
+        {
+            var invalidCountryCode = "ZZ";
+            var response = await Client.GetAsync($"{_apiRelativePath}/{invalidCountryCode}");
+
+            Assert.That(response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
         }
     }
 }
